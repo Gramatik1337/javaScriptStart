94da384174f7a6f6d9a2fa0a19d65bd6f8511e35
Gramatik1337 <160417101+Gramatik1337@users.noreply.github.com>
2024-04-16 10:51:14 +0200

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,123 @@
+// const obj = {
+//   name: "John",
+//   age: 25,
+//   isMarried: false,
+// };
+
+// console.log(obj.name);
+
+// const arr = ["a", "b", "c"];
+// const arrObj = {
+//   a: "a",
+//   1: "b",
+//   3: "c",
+// };
+
+// arrObj.b = "1234";
+// console.log(arrObj["b"]);
+// console.log(arrObj.b);
+
+// const result = confirm("are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt("как ваше имя?", "");
+// answers[1] = prompt("как ваша фамилия?", "");
+// answers[2] = prompt("сколько вам лет??", "");
+
+// document.write(answers);
+
+// const category = "toys";
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false,
+// };
+
+// const a = prompt("Один из последних просмотренных фильмов", ""),
+//   b = prompt("На сколько оцените его?", ""),
+//   c = prompt("Один из последних просмотренных фильмов", ""),
+//   d = prompt("На сколько оцените его?", "");
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
+
+// if (4 == 4) {
+//   console.log("Ok!");
+// } else {
+//   console.log("error");
+// }
+
+// if (num < 49) {
+//   console.log("error");
+// } else if (num > 100) {
+//   console.log("много");
+// } else {
+//   console.log("Ok!");
+// }
+
+// num === 50 ? console.log("Ok!") : console.log("error");
+
+// const num = 50;
+
+// switch (num) {
+//   case 49:
+//     console.log("Неверно");
+//     break;
+//   case 100:
+//     console.log("Неверно");
+//     break;
+//   case 50:
+//     console.log("В точку");
+//     break;
+//   default:
+//     console.log("Не в этот раз");
+//     break;
+// }
+
+// const hamburger = true;
+// const fries = true;
+
+// if (hamburger && fries) {
+//   console.log("Я сыт");
+// }
+
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 0;
+
+// console.log(hamburger === 3 && cola && fries);
+
+// if (hamburger === 3 && cola === 2 && fries) {
+//   console.log("Все сыты");
+// } else {
+//   console.log("Мы уходим");
+// }
+
+// let johnReport, jackReport, samReport, mariaReport = "done";
+
+// console.log(johnRepor || jackReport || samReport || mariaReport);
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+// if ((hamburger === 3 && cola === 2) || (fries === 3 && nuggets)) {
+//   console.log("Все довольны");
+// } else {
+//   console.log("Мы уходим");
+// }
